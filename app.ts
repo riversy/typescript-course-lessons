33159b3257a617bccfdbcbd802c2a7431ab2d5c7
Igor Goltsov <riversy@gmail.com>
2022-10-13 09:04:20 +0300

    Use objects and types with functions

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-const a = 1;
+function getFullName(user: {firstname: string, lastname: string}): string {
+    return `${user.firstname} ${user.lastname}`;
+}
+
+
+const user = {
+    firstname: 'Ihar',
+    lastname: 'Haltsou',
+    city: 'Mogilev',
+    age: 38,
+}
+
+
+console.log(getFullName(user));
+
+
+
